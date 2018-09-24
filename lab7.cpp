7757ec194b16a7fd57791ccfa1ac23b50c85a2ca
twishajain <33196561+twishajain@users.noreply.github.com>
2018-09-24 10:53:18 +0530

    Update lab7.cpp

--- lab7.cpp
+++ lab7.cpp
@@ -10,6 +10,8 @@ public:
     void display();
     void ins_after();
     void delSpecific();
+    void reversing();
+    void sortt();
 };
 SLL *first=NULL, *last=NULL;
 void SLL::ins_end()
@@ -117,17 +119,54 @@ void SLL::delSpecific()
         }
     }
 }
+void SLL::reversing()
+{
+    SLL *curr,*prev;
+    curr=prev=NULL;
+    while (first!=NULL)
+    {
+        prev=curr;
+        curr=first;
+        first=first->link;
+        curr->link=prev;
+    }
+    first=curr;
+}
+void SLL::sortt()
+{
+    SLL *i,*j;int temp;
+    for (i=first;i->link!=NULL;i=i->link)
+    {
+        for (j=i->link;j!=NULL;j=j->link)
+        {
+            if (i->data>j->data)
+            {
+                temp=i->data;i->data=j->data;
+                j->data=temp;
+            }
+        }
+    }
+}
 int main()
 {
     SLL k;
-    k.ins_end();
-    k.ins_end();
-    k.ins_end();
-    k.display();
-    k.ins_before();
-    k.display();
-    k.ins_after();
-    k.display();
-    k.delSpecific();
-    k.display();
+    int option;
+    char ch;
+    do{
+    cout<<"1.Insert_end 2.Insert_before 3.Insert_after 4.Reverse 5.Delete 6.Sort 7.Display";
+    cin>>option;
+    switch(option)
+    {
+        case 1: k.ins_end();break;
+        case 2:k.ins_before();break;
+        case 3:k.ins_after();break;
+        case 4:k.reversing();break;
+        case 5:k.delSpecific();break;
+        case 6:k.sortt();break;
+        case 7:k.display();break;
+    }
+    cout<<"Do you want to continue?";
+    cin>>ch;
+
+}while (ch=='y'|| ch=='Y');
 }
